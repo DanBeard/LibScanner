19b5573353d262840a35f9afdfad88b88d4ac1d1
Daniel Beard <daniel@promenadesoftware.com>
2017-02-22 15:03:42 -0800

    Added 2017 xml file

--- download_xml.sh
+++ download_xml.sh
@@ -4,7 +4,7 @@ mkdir -p dbs
 cd dbs
 rm *.xml
 
-for i in $(seq -f "%02g" 2 16)
+for i in $(seq -f "%02g" 2 17)
 do
     wget https://nvd.nist.gov/download/nvdcve-20$i.xml.gz
     gunzip nvdcve-20$i.xml.gz
